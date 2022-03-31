0aeb6026e9acafe1df6da4ee13b8150101e2d2f2
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-03-31 08:15:59 +0300

    emoji picker init and working

--- components/TweetBox.js
+++ components/TweetBox.js
@@ -3,12 +3,25 @@ import Gif from '../assets/icons/Gif.svg';
 import Emoji from '../assets/icons/Emoji.svg';
 import Location from '../assets/icons/Location.svg';
 
+import 'emoji-mart/css/emoji-mart.css';
+import {Picker} from 'emoji-mart';
+
 import {useState, useRef} from 'react';
 
 function TweetBox() {
   const [input, setInput] = useState('');
   const [item, setItem] = useState('');
   const imageSelect = useRef(null);
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split('-');
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push('0x' + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
+
   return (
     <div className='flex px-4 py-1 h-fit border-b border-gray-700 '>
       <div className='  pt-1 flex items-start justify-center'>
@@ -24,7 +37,7 @@ function TweetBox() {
           <textarea
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            placeholder="What's Happening?"
+            placeholder="What's happening?"
             className=' bg-transparent min-h-[75px] outline-none text-[#d9d9d9] text-xl placeholder-gray-500 tracking-wide  overflow-hidden resize-none p-2 '
           />
         </form>
@@ -42,11 +55,28 @@ function TweetBox() {
               <Gif className='fill-orange-500 w-5 h-5 hover:cursor-pointer hover:opacity-80' />
             </div>
             <div className='w-fit'>
-              <Emoji className='fill-orange-500 w-5 h-5 hover:cursor-pointer hover:opacity-80' />
+              <Emoji
+                className='fill-orange-500 w-5 h-5 hover:cursor-pointer hover:opacity-80'
+                onClick={() => setChosenEmoji(!chosenEmoji)}
+              />
             </div>
             <div className='w-fit'>
               <Location className='fill-orange-500 w-5 h-5 hover:cursor-pointer hover:opacity-80' />
             </div>
+
+            {chosenEmoji && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: 'absolute',
+                  marginTop: '465px',
+                  marginLeft: '40px',
+                  maxWidth: '320px',
+                  borderRadius: '30px',
+                }}
+                theme='dark'
+              />
+            )}
           </div>
           <button className=' w-22 px-4 bg-orange-500 rounded-full h-[34px]  font-bold text-lg hover:opacity-80 active:scale-90 transition-all select-none appearance-none'>
             Tweet
