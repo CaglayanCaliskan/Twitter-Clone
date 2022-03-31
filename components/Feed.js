bcfe230831b51450a760ca7c636008297ae53623
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-03-31 23:59:19 +0300

    fetch loop problem solved

--- components/Feed.js
+++ components/Feed.js
@@ -2,30 +2,30 @@ import {useEffect} from 'react';
 import TopHeader from './TopHeader';
 import Tweet from './Tweet';
 import TweetBox from './TweetBox';
-import {collection, getDocs} from 'firebase/firestore';
+import {collection, onSnapshot, orderBy, query} from 'firebase/firestore';
 import {db} from '../firebase.config';
 import {useState} from 'react';
 
 function Feed() {
   const [tweets, setTweets] = useState([]);
 
-  const fetchTweets = async () => {
-    const querySnapshot = await getDocs(collection(db, 'tweets'));
-    let semiTweet = [];
-    querySnapshot.forEach((doc) => {
-      semiTweet.push(doc.data());
-    });
-    setTweets(semiTweet);
-  };
-
-  useEffect(fetchTweets, [tweets]);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'tweets'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setTweets(snapshot.docs);
+        }
+      ),
+    [db]
+  );
 
   return (
     <div className='flex-grow border-l border-r border-gray-700 ml-[52px] sm:ml-[92px] md:ml-[128px] xl:ml-[275px]  text-cyan-50'>
       <TopHeader />
-      <TweetBox onSubmitTweet={fetchTweets} />
+      <TweetBox />
       {tweets.map((tweet, index) => (
-        <Tweet key={index} tweet={tweet} />
+        <Tweet key={index} tweet={tweet.data()} />
       ))}
     </div>
   );
